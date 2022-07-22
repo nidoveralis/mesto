2c145ee80b4dc0772d33ccbd49e6528eb60a7f70
Nidoveralis <nidoveralis@gmail.com>
2022-07-22 17:40:12 +0400

    Create Api.js

---
+++ src/components/Api.js
@@ -0,0 +1,17 @@
+export class Api{
+  constructor() {
+
+  };
+
+  getInitialCards() {
+
+  };
+};
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+    'Content-Type': 'application/json'
+  }
+}); 
